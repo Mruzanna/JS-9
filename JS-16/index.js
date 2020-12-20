1e0ebf55ca3803772baff12a0e4a08cc39f3c73b
Mruzanna <64004620+Mruzanna@users.noreply.github.com>
2020-12-20 20:06:31 +0400

    ADD FILE

---
+++ JS-16/index.js
@@ -0,0 +1,15 @@
+
+const btn = document.getElementById('btn')
+const p = document.getElementById('text')
+const h3 = document.getElementById('title')
+const img=document.getElementById('card1')
+
+
+ function handleClick () {
+   p.style.color = '#800080'
+   card1.style.borderRadius = '30px' 
+   h3.style.color ='#0000ff'
+   
+}
+
+
